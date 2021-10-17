c02614a025736ea2c820e0ea82e8872f40c1a994
inamulyajmi <inamulyajmi@gmail.com>
2021-10-17 20:14:21 +0700

    first commit

---
+++ JavaBasic3Challange.java
@@ -0,0 +1,13 @@
+public class JavaBasic3Challange{
+    public static void main(String[] args){
+
+        int i,j;
+        for(i = 0; i < 10; i++){
+            for(j = 0; j < i; j++){
+                System.out.print(" 0 ");
+            }
+            System.out.println();
+        }
+
+    }
+}
